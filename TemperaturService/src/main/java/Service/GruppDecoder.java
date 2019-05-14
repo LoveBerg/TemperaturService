92c54235eee36fcdfe390eb3927c9c483546c6da
jofen <joel.fandriks@gmail.com>
2019-05-14 14:06:04 +0200

    grunderna tillagda

--- TemperaturService/src/main/java/Service/GruppDecoder.java
+++ TemperaturService/src/main/java/Service/GruppDecoder.java
@@ -1,14 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Service;
 
-/**
- *
- * @author loveo
- */
-public class GruppDecoder {
-    String korv;
+import javax.websocket.DecodeException;
+import javax.websocket.Decoder;
+import javax.websocket.EndpointConfig;
+import Sensor.Sensor;
+import com.google.gson.Gson;
+
+
+
+public class GruppDecoder implements Decoder.Text<Sensor> {
+
+    @Override
+    public Sensor decode(String s) throws DecodeException {
+        Gson gson = new Gson();
+        Sensor sensor = gson.fromJson(s, Sensor.class);
+        return  sensor;    
+    }
+
+    @Override
+    public boolean willDecode(String s) {
+          return (s != null);
+            
+        }
+
+    @Override
+    public void init(EndpointConfig endpointConfig) {
+        System.out.println("init");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy");
+    }
+    
 }
